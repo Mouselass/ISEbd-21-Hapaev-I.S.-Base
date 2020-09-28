77004d2fe2237ab1ca4dbaea93e418b6940cbabd
Mouselass <ivan.hapaev@yandex.ru>
2020-09-28 16:24:48 +0400

    WaterPlane

---
+++ Seaplane/Seaplane/FormPlane.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Seaplane
+{
+    partial class FormPlane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreatePlane = new System.Windows.Forms.Button();
+            this.buttonCreateWaterPlane = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.pictureBoxPlane = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlane)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreatePlane
+            // 
+            this.buttonCreatePlane.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreatePlane.Name = "buttonCreatePlane";
+            this.buttonCreatePlane.Size = new System.Drawing.Size(120, 32);
+            this.buttonCreatePlane.TabIndex = 1;
+            this.buttonCreatePlane.Text = "Создать самолет";
+            this.buttonCreatePlane.UseVisualStyleBackColor = true;
+            this.buttonCreatePlane.Click += new System.EventHandler(this.ButtonCreatePlane_Click);
+            // 
+            // buttonCreateWaterPlane
+            // 
+            this.buttonCreateWaterPlane.Location = new System.Drawing.Point(138, 12);
+            this.buttonCreateWaterPlane.Name = "buttonCreateWaterPlane";
+            this.buttonCreateWaterPlane.Size = new System.Drawing.Size(135, 32);
+            this.buttonCreateWaterPlane.TabIndex = 2;
+            this.buttonCreateWaterPlane.Text = "Создать гидросамолет";
+            this.buttonCreateWaterPlane.UseVisualStyleBackColor = true;
+            this.buttonCreateWaterPlane.Click += new System.EventHandler(this.ButtonCreateWaterPlane_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackgroundImage = global::Seaplane.Properties.Resources.arrowLeft;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(755, 408);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackgroundImage = global::Seaplane.Properties.Resources.arrowRight;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(683, 408);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackgroundImage = global::Seaplane.Properties.Resources.arrowDown;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(719, 408);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 6;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.BackgroundImage = global::Seaplane.Properties.Resources.arrowUp;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(719, 372);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 4;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxPlane
+            // 
+            this.pictureBoxPlane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPlane.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxPlane.Name = "pictureBoxPlane";
+            this.pictureBoxPlane.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxPlane.TabIndex = 0;
+            this.pictureBoxPlane.TabStop = false;
+            // 
+            // FormPlane
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonCreateWaterPlane);
+            this.Controls.Add(this.buttonCreatePlane);
+            this.Controls.Add(this.pictureBoxPlane);
+            this.Name = "FormPlane";
+            this.Text = "FormPlane";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlane)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxPlane;
+        private System.Windows.Forms.Button buttonCreatePlane;
+        private System.Windows.Forms.Button buttonCreateWaterPlane;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+    }
+}
---
+++ Seaplane/Seaplane/WaterPlane.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Seaplane
+{
+    public class WaterPlane : Plane
+    {
+        public Color DopColor { private set; get; }
+
+        public bool Star { private set; get; }
+
+        public bool Wing { private set; get; }
+
+        public bool Floater { private set; get; }
+
+        public WaterPlane(int maxSpeed, float weight, Color mainColor, Color dopColor, bool star, bool wing, bool floater) :
+            base(maxSpeed, weight, mainColor, 130, 60)
+        {
+            DopColor = dopColor;
+            Star = star;
+            Wing = wing;
+            Floater = floater;
+        }
+
+        public override void DrawTransport(Graphics g)
+        {
+            base.DrawTransport(g);
+
+            if (Star)
+            {
+                Brush star = new SolidBrush(DopColor);
+
+                PointF s1 = new PointF(_startPosX + 85, _startPosY + 24);
+                PointF s2 = new PointF(_startPosX + 92, _startPosY + 22);
+                PointF s3 = new PointF(_startPosX + 100, _startPosY + 17);
+                PointF s4 = new PointF(_startPosX + 108, _startPosY + 22);
+                PointF s5 = new PointF(_startPosX + 115, _startPosY + 24);
+                PointF s6 = new PointF(_startPosX + 107, _startPosY + 29);
+                PointF s7 = new PointF(_startPosX + 110, _startPosY + 33);
+                PointF s8 = new PointF(_startPosX + 100, _startPosY + 31);
+                PointF s9 = new PointF(_startPosX + 90, _startPosY + 34);
+                PointF s10 = new PointF(_startPosX + 92, _startPosY + 29);
+
+                PointF[] starP = { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10 };
+                g.FillPolygon(star, starP);
+            }
+
+            if (Wing)
+            {
+                Brush wing = new SolidBrush(DopColor);
+
+
+                PointF w6 = new PointF(_startPosX + 50, _startPosY + 13);
+                PointF w7 = new PointF(_startPosX + 80, _startPosY + 33);
+                PointF w8 = new PointF(_startPosX + 87, _startPosY + 28);
+                PointF w9 = new PointF(_startPosX + 55, _startPosY + 10);
+                PointF w10 = new PointF(_startPosX + 50, _startPosY + 13);
+
+                PointF[] dopWingP = { w6, w7, w8, w9, w10 };
+                g.FillPolygon(wing, dopWingP);
+            }
+
+            if (Floater)
+            {
+                Brush floater = new SolidBrush(DopColor);
+
+                PointF f1 = new PointF(_startPosX + 30, _startPosY + 40);
+                PointF f2 = new PointF(_startPosX + 30, _startPosY + 45);
+                PointF f3 = new PointF(_startPosX, _startPosY + 45);
+                PointF f4 = new PointF(_startPosX + 10, _startPosY + 55);
+                PointF f5 = new PointF(_startPosX + 70, _startPosY + 55);
+                PointF f6 = new PointF(_startPosX + 120, _startPosY + 45);
+                PointF f7 = new PointF(_startPosX + 70, _startPosY + 45);
+                PointF f8 = new PointF(_startPosX + 70, _startPosY + 40);
+                PointF f9 = new PointF(_startPosX + 65, _startPosY + 40);
+                PointF f10 = new PointF(_startPosX + 65, _startPosY + 45);
+                PointF f11 = new PointF(_startPosX + 35, _startPosY + 45);
+                PointF f12 = new PointF(_startPosX + 35, _startPosY + 40);
+                PointF f13 = new PointF(_startPosX + 30, _startPosY + 40);
+
+                PointF[] floaterP = { f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13 };
+                g.FillPolygon(floater, floaterP);
+            }
+        }
+    }
+}
